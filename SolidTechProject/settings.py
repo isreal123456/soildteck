85cfe3c3638d65313b2679fe92bf518732d184c3
isreal123456 <sundayisrael41@gmail.com>
2024-01-26 11:38:30 +0100

    Update settings.py

--- SolidTechProject/settings.py
+++ SolidTechProject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g0mp*-^c(i9-=3+^%#a$5cwob8$@(dvwwr3nx85s9f88$k1%n='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
